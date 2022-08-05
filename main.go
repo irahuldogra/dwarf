c32998f27c2e22a157ccfc263850ed32c8a9c4de
Rahul Dogra <rahuldogra1998@gmail.com>
2022-08-06 03:03:42 +0530

    :sparkles: GetAllDwarves Api

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "shortener/model"
+
+func main() {
+	model.Setup()
+}
---
+++ server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"shortener/model"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func getAllRedirects(ctx *fiber.Ctx) error {
+	dwarves, err := model.GetAllDwarves()
+
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "error getting all dwarves links" + err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(dwarves)
+}
+
+func SetupAndListen() {
+
+	router := fiber.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	router.Get("/dwarf", getAllRedirects)
+
+}
