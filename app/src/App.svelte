aa9db8c86e29f23d9b141e6e8efba0326f6e9c7c
Rahul Dogra <rahuldogra1998@gmail.com>
2022-08-07 05:57:24 +0530

    :truck: remove app.css

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -8,6 +8,22 @@
 </main>
 
 <style>
+  :root {
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    font-family: 'Open Sans', sans-serif;
+
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
   .header {
     margin-left: 10px;
     letter-spacing: 2px;
